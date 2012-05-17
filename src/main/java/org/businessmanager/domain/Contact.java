dedb1477d6032b34e4118b88743d3b4735dbda3a
Thorsten <t.volland@web.de>
2012-05-17 11:24:31 +0200

    Resolve conflicts

--- src/main/java/org/businessmanager/domain/Contact.java
+++ src/main/java/org/businessmanager/domain/Contact.java
@@ -1,6 +1,7 @@
 package org.businessmanager.domain;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -14,6 +15,7 @@ import javax.persistence.OneToMany;
 
 import org.apache.commons.collections.Predicate;
 import org.apache.commons.lang3.StringUtils;
+import org.businessmanager.domain.Address.AddressType;
 import org.businessmanager.util.CollectionUtil;
 import org.businessmanager.util.DefaultItemPredicate;
 
@@ -48,11 +50,14 @@ public class Contact extends AbstractEntity {
 
 	@Column(length=20)
 	private String salutation;
+	
+	@Column
+	private Calendar birthday;
 
 	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "contact")
 	private List<ContactItem> contactItems = new ArrayList<ContactItem>();
 
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "contact")
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "contact")
 	private List<Address> addresses = new ArrayList<Address>();
 
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "contact")
@@ -311,7 +316,7 @@ public class Contact extends AbstractEntity {
 							public boolean evaluate(Object paramObject) {
 								if (paramObject instanceof Address) {
 									Address address = (Address) paramObject;
-									return Address.SCOPE_BILLING.equals(address
+									return AddressType.SCOPE_BILLING.equals(address
 											.getScope());
 								}
 								return false;
@@ -333,7 +338,7 @@ public class Contact extends AbstractEntity {
 							public boolean evaluate(Object paramObject) {
 								if (paramObject instanceof Address) {
 									Address address = (Address) paramObject;
-									return Address.SCOPE_SHIPPING
+									return AddressType.SCOPE_SHIPPING
 											.equals(address.getScope());
 								}
 								return false;
@@ -360,5 +365,13 @@ public class Contact extends AbstractEntity {
 			sb.append(value);
 			sb.append(" ");
 		}
+	}
+
+	public void setBirthday(Calendar birthday) {
+		this.birthday = birthday;
+	}
+
+	public Calendar getBirthday() {
+		return birthday;
 	}
 }
